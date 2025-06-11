b9dcb610dcc2b1919c10d4d3eaaac882edf75453
priyanshu2393 <priyanshuprajapati783@gmail.com>
2025-06-11 23:51:32 +0530

    allow multiple user with same username

--- Backend/auth/dbmodel.py
+++ Backend/auth/dbmodel.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, nullable=False, index=True)
+    username = Column(String,  nullable=False, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
